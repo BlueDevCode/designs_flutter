66392edfda9779870f24040b36b837da1e533653
Claudia Cadec <claudiacadec@outlook.com>
2023-02-25 20:59:10 +0100

    commit

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -11,14 +11,29 @@ class BasicDesignScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent,
       body:Column(
-        children: const [
-           Image(image: AssetImage("assets/landscape.jpg")),
-           Title()
+        children:  [
+           const Image(image: AssetImage("assets/landscape.jpg")),
+           Title(),
+           const ButtonSection(),
+           Container(
+             margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+             child: const Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+          
+             ),
+           ),
+
         ],
       )
     );
   }
 }
+
 
 class Title extends StatelessWidget {
   const Title({
@@ -28,7 +43,7 @@ class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
       child: Row(
        
        children:  [
@@ -36,6 +51,7 @@ class Title extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
              Text("Oeschine lake campground", style: TextStyle(fontWeight: FontWeight.bold),),
+             SizedBox(height: 10,),
              Text ("Kandersteg, Switzerland",style: TextStyle(color:Colors.black54),)
           ],
         ),
@@ -44,6 +60,57 @@ class Title extends StatelessWidget {
          const Text("41")
        ],
       ),
+    );
+  }
+}
+
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+      child: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
+       children: const [
+            CustomButton(icon: Icons.call, text: "CALL"),
+            CustomButton(icon: Icons.near_me, text: "ROUTE"),
+            CustomButton(icon: Icons.share, text: "SHARE"),
+          ],
+        ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  
+  const CustomButton({
+    Key? key,
+     required this.icon,
+     required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:  [
+        Icon(icon, 
+        color: const Color.fromARGB(255, 4, 54, 5), size:30),
+        const SizedBox(height: 10,),
+         Text(text,
+         style: const TextStyle(
+         color: Color.fromARGB(255, 4, 54, 5),
+        fontWeight: FontWeight.bold
+          )),
+          
+      ],
     );
   }
 }
